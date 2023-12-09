a923daed9ee8edec256aea1713aee14a477104f3
leopoldocarlos <leopoldo@uazuay.edu.ec>
2023-12-09 12:55:31 -0500

    initial commit

---
+++ tablero_mensajes/urls.py
@@ -0,0 +1,8 @@
+# tablero_mensajes/urls.py
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("", include("posts.urls")),
+]
